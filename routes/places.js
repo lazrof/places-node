5e762a7e5b316a9e19cd1e5451a1a55ea8cc0ca1
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-06-20 16:51:55 -0500

    Controladores - add PlacesController in controllers folder

--- routes/places.js
+++ routes/places.js
@@ -1,78 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const Place = require('../models/Place');
+const placesController = require('../controllers/PlacesController');
 
 router.route('/')
-    .get( (req, res) => {
-        
-        Place.find({})
-            .then(docs => {
-                res.json(docs);
-            }).catch(err => {
-                console.log(err);
-                res.json(err);
-            });
-      
-    })
-    .post( (req, res) => {
-
-        Place.create({
-            title:req.body.title,
-            description: req.body.description,
-            acceptsCreditCard: req.body.acceptsCreditCard,
-            openHour: req.body.openHour,
-            closeHour: req.body.closeHour
-        }).then(doc => {
-            res.json(doc);
-        }).catch(err => {
-            console.log(err);
-            res.json(err);
-        });
-    });
+    .get(placesController.index)
+    .post(placesController.create);
 
 router.route('/:id')
-    .get( (req, res) => {
-        
-        Place.findById(req.params.id)
-            .then(doc => {
-            res.json(doc);
-            }).catch(err =>{
-            console.log(err);
-            res.json(err);
-        });
-
-    })
-    .put((req, res) => {
-
-        let attributes = ['title', 'description', 'acceptsCreditCard', 'openHour', 'closeHour']
-        let placeParams = {}
-        
-        attributes.forEach(attr => {
-          if(Object.prototype.hasOwnProperty.call(req.body, attr)){
-            placeParams[attr] = req.body[attr];
-          }
-        });
-        
-        Place.findByIdAndUpdate({'_id': req.params.id}, placeParams, {new:true})
-        .then(doc => {
-          res.json(doc);
-        }).catch(err => {
-          console.log();
-          res.json(err);
-        });
-
-    })
-    .delete(  (req, res) => {
-
-        Place.findByIdAndRemove(req.params.id)
-          .then(doc => {
-            res.json({});
-        }).catch(err => {
-            console.log();
-            res.json(err);
-        });
-
-    })
+    .get(placesController.show)
+    .put(placesController.update)
+    .delete(placesController.destroy)
 
 module.exports = router; 
